1f5dbae3ea856229f783346c76a768b22b8c3ef2
Mariusz Ostrowski <mariusz.ostrowski@mardom.com.pl>
2020-11-01 17:41:23 +0100

    Zadanie HelloWorld

---
+++ kodilla-intro/src/main/java/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Mariusz");
+        System.out.println("43");
+        System.out.println("O");
+    }
+}
