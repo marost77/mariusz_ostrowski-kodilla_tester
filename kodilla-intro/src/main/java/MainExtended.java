d62c1413932770886e0ccee3345f6e94bf323b98
Mariusz Ostrowski <mariusz.ostrowski@mardom.com.pl>
2020-11-01 20:22:20 +0100

    MainExtended

---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,15 @@
+public class MainExtended {
+    public static void main(String[] args){
+        String myDoughterName="Julia";
+        int myDoughterAge = 40;
+        double myDoughterHeight = 1.45;
+        char myDoughterClass = '4';
+        boolean isGoodStudent = true;
+        String myDoughterBestMovie = "Harry Potter";
+            System.out.println(myDoughterName);
+            System.out.println(myDoughterHeight);
+            System.out.println(myDoughterClass);
+            System.out.println(isGoodStudent);
+            System.out.println(myDoughterBestMovie);
+    }
+}
