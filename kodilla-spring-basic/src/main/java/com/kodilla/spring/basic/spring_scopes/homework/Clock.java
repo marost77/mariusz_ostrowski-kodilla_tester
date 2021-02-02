7611f060b359955bada765964d358a737ca6183f
marost77 <mariuszostrowski77@gmail.com>
2021-02-02 17:09:58 +0100

    Zadanie 9.5. Zasięgi beanów

---
+++ kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/spring_scopes/homework/Clock.java
@@ -0,0 +1,17 @@
+package com.kodilla.spring.basic.spring_scopes.homework;
+
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalTime;
+
+@Component
+@Scope("prototype")
+public class Clock {
+
+    public LocalTime showTime(){
+        return LocalTime.now();
+
+    }
+}
---
+++ kodilla-spring-basic/src/test/java/com/kodilla/spring/basic/spring_scopes/homework/ClockTestSuite.java
@@ -0,0 +1,29 @@
+package com.kodilla.spring.basic.spring_scopes.homework;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.time.LocalTime;
+
+@SpringBootTest
+class ClockTestSuite {
+
+    @Test
+    public void shouldShowTimeNow(){
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring");
+        Clock clock1 = context.getBean(Clock.class);
+
+        LocalTime expectedTime = LocalTime.now();
+        LocalTime test1Time = clock1.showTime();
+        LocalTime test2Time = clock1.showTime();
+        LocalTime test3Time = clock1.showTime();
+
+        Assertions.assertNotEquals(expectedTime,test1Time);
+        Assertions.assertNotEquals(expectedTime, test2Time);
+        Assertions.assertNotEquals(expectedTime, test3Time);
+    }
+
+}
